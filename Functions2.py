d18cc787542619467f4934b6b5cd082a512525ab
Samanyu Thakur <aaravthakur1231@gmail.com>
2025-09-28 22:53:38 +0530

    as

---
+++ Functions2.py
@@ -0,0 +1,22 @@
+def hello10():
+    print('Hello'* 10 )
+
+def pyramid(layers):
+    star ='*'
+    for i in range (0,layers):
+        print (star.center(layers * 2, ' '))
+        star += '**'
+
+def circle_area(radius):
+    import math
+    return math.pi * radius ** 2
+
+def surface_area_cuboid(l,w,h):
+    return 2 * (l*w + w*h + l*h)
+
+if __name__=='_main_':
+    hello10
+    pyramid(8)
+    print((circle_area(5)))
+    print(surface_area_cuboid(3,5,7))
+    print(surface_area_cuboid.__doc__)
